95ec15861b76b24b716a140b074815e39efdd17d
Andrey Pakhomov <pahomeathome@gmail.com>
2022-11-07 04:46:15 +0200

    methods added

--- src/app/domain/person/Person.ts
+++ src/app/domain/person/Person.ts
@@ -1,12 +1,27 @@
-import { JsonController, Get } from "routing-controllers";
+import { JsonController, Get, Post, Body, Param } from "routing-controllers";
 
 import { App } from "infra/App";
+import PersonService from "./PersonService";
+import { IPerson } from "./PersonType";
 
-@JsonController()
+@JsonController("/person")
 export default class Person {
   public app = new App();
-  @Get("/person/getHello")
-  async getHello() {
-    return "Hello";
+  public service = new PersonService();
+
+  @Get()
+  async getAll() {
+    return await this.service.getAll();
+  }
+
+  @Get("/:id")
+  async getOne(@Param("id") id: number) {
+    return await this.service.getOne(id);
+  }
+
+  @Post()
+  async setPerson(@Body() body: IPerson) {
+    await this.service.setPerson(body);
+    return "Person added";
   }
 }
